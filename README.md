bb306e57fe941e84c8470593ce3d3bf0694fef78
Teibidh <teibidh@gmail.com>
2019-06-08 01:48:58 -0400

    Several structure updates, one partially completed example block that I only noticed when I went to commit. Oops.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SpiritIO
 Discover ancient secrets and use them to enlist, command, and empower denizens of the The Akasha (spirit realm)!
+
+[![Discord](https://img.shields.io/discord/585873005012582410.svg)](https://discord.gg/DHRFTMx)
--- src/main/java/io/teibidh/SpiritIO/SpiritIO.java
+++ src/main/java/io/teibidh/SpiritIO/SpiritIO.java
@@ -1,43 +1,42 @@
 package io.teibidh.spiritio;
 
+import io.teibidh.spiritio.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = SpiritIO.modId, name = SpiritIO.name, version = SpiritIO.version)
+@Mod(modid = SpiritIO.MODID, name = SpiritIO.MODNAME, version = SpiritIO.MODVERSION, useMetadata = true, certificateFingerprint = "e157e8b293e545725d6a067ee1a46664f173e8d8")
 public class SpiritIO
 {
-    public static final String modId = "spiritio";
-    public static final String name = "SpiritIO";
-    public static final String version = "@VERSION@";
 
-    private static Logger logger;
+    public static final String MODID = "spiritio";
+    public static final String MODNAME = "SpiritIO";
+    public static final String MODVERSION= "0.0.2";
 
-    @Mod.Instance(modId)
+    @SidedProxy(clientSide = "io.teibidh.spiritio.proxy.ClientProxy", serverSide = "io.teibidh.spiritio.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance
     public static SpiritIO instance;
 
+    public static Logger logger;
+
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        // Initialize logger and declare ourselves aware of preInit.
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
-        logger.info("Tearing open a Vivara... <Warming up for init>");
+        proxy.preInit(event);
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-       // Declare ourselves initializing.
-        logger.info("Vivara is fully dialated, step through to The Akasha... <Initializing>");
+    public void init(FMLInitializationEvent e) {
+        proxy.init(e);
     }
 
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-        // Declare our initialization finished, hopefully successfully.
-        logger.info("Your body melts in to the mists of The Akasha. Good luck! <Init done>");
+    public void postInit(FMLPostInitializationEvent e) {
+        proxy.postInit(e);
     }
-
 }
